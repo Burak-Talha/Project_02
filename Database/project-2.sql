3edd6ebca28539b2b80675d846f43688fc44c6c4
Burak Talha <76775662+Burak-Talha@users.noreply.github.com>
2021-10-21 23:28:51 +0300

    Update project-2.sql

--- Database/project-2.sql
+++ Database/project-2.sql
@@ -3,26 +3,12 @@ use project_2;
 
 create table users (
 id int primary key auto_increment,
+discord_id long,
 username varchar(50) not null,
-password varchar(50) not null
+password varchar(50) null
 );
 
-create table ranks(
-id int primary key auto_increment,
-rank_name varchar(70) not null,
-rank_explanation text not null
-);
-
-create table users_rank(
-id int primary key auto_increment,
-users_id int not null,
-rank_id int not null,
-
-CONSTRAINT users_rank_fk_01 FOREIGN KEY (users_id) references users(id),
-CONSTRAINT users_rank_fk_02 FOREIGN KEY (rank_id) references ranks(id)
-);
-
-create table subject_category(
+create table subjectcategory(
 id int primary key auto_increment,
 subject_category varchar(50)
 );
@@ -32,10 +18,10 @@ id int auto_increment primary key,
 subject_name varchar(50),
 subject_category_id int,
 
-CONSTRAINT subject_ibfk_01 foreign key (subject_category_id) references subject_category(id)
+CONSTRAINT subject_ibfk_01 foreign key (subject_category_id) references subjectcategory(id)
 );
 
-create table subject_options(
+create table subjectoptions(
 id int primary key auto_increment,
 id_subject int,
 subject_option varchar(30),
@@ -43,13 +29,27 @@ subject_options_vote int check(subject_options_vote >= 0),
 
 CONSTRAINT subject_options_ibfk_01 foreign key (id_subject) references subject(id)
 );
-
-create table users_vote (
+	
+create table usersvote (
 id int primary key auto_increment,
 users_id int,
 subject_options_id int,
 added_at datetime,
 
 CONSTRAINT users_vote_ibfk_01 foreign key (users_id) references users(id),
-CONSTRAINT users_vote_ibfk_02 foreign key (subject_options_id) references subject_options(id)
+CONSTRAINT users_vote_ibfk_02 foreign key (subject_options_id) references subjectoptions(id)
+);
+
+create table sweepstake (
+id int primary key auto_increment,
+sweepstake_subject varchar(50) not null,
+number_of_persons int check(number_of_persons >= 0)
+);
+
+create table sweepstakewinners(
+id int primary key auto_increment,
+users_id int,
+comments varchar(500),
+
+CONSTRAINT sweepstakewinners_ibfk_01 foreign key (users_id) references users(id)
 );
